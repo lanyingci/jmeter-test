c3bf3cd4d2d288dcec6d54e681e205517b11992b
lanyingci <651094581@qq.com>
2016-09-17 20:28:00 +0800

    jmeter接口测试

---
+++ src/main/java/zhihu/login/App.java
@@ -0,0 +1,13 @@
+package zhihu.login;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
